d8b285e01932c5b01c2a7de58f57672a65f8bc9f
Jonatan Compiani <jonatan.compiani@gmail.com>
2024-02-19 09:04:21 -0300

    Allowed parametrization of content

--- ResumeBuilder.Console/Program.cs
+++ ResumeBuilder.Console/Program.cs
@@ -1,8 +1,16 @@
 ﻿using QuestPDF.Infrastructure;
 using ResumeBuilder.Application;
+using System.Text.Json;
 
 QuestPDF.Settings.License = LicenseType.Community;
 
-Generator gen = new(new(Theme.Cyan));
+
+var c = new Content(Theme.Green);
+
+c.Name = "Potato Batata";
+
+string serialized = JsonSerializer.Serialize(c);
+
+Generator gen = new(c);
 gen.PreviewFile();
 
